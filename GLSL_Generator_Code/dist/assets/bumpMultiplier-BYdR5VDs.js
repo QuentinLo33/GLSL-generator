2203d9cbeea56ebaafb1344842d95edc0d575357
qlopez <qlopez@ensc.fr>
2026-03-30 12:09:28 +0200

    try deploy3

--- GLSL_Generator_Code/dist/assets/bumpMultiplier-BYdR5VDs.js
+++
@@ -1,4 +0,0 @@
-class o{constructor(t,{input:e="vPosition",factor:i=1}={}){this.name=t,this.input=e,this.factor=i}generateCodeGlobal(){return""}generateCodeMain(){return`    // BUMP MAIN: ${this.name}
-    vec3 ${this.name} = ${this.input} * ${this.factor.toFixed(2)};
-    
-`}}export{o as B};
